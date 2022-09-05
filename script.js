34023669614dde756b92b51d2e9a9da81c654cf5
Daron Wiafe <daronkwiafe@gmail.com>
2022-09-05 16:32:21 +0200

    Style mobile menu and add close button with javascript

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const toolbarBtn = document.getElementById('toolbar-btn');
 
 const menu = document.getElementById('menuOpen');
 const toolbar = document.getElementById('navigation');
+const cancelBtn = document.getElementById('closeBtn');
 
 function menuOpen() {
-    menu.style.display = "block";
+    menu.style.display = "flex";
     toolbar.style.display = "none";
+}
+
+function menuClose() {
+    menu.style.display = "none";
+    toolbar.style.display = "flex";
 }
