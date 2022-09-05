f065b4f9869b707b6597a4887b964ea14716bf78
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-05 16:39:22 +0300

    add new menu items/ javascript sheet

---
+++ script.js
@@ -0,0 +1,9 @@
+const toolbarBtn = document.getElementById('toolbar-btn');
+
+const menu = document.getElementById('menuOpen');
+const toolbar = document.getElementById('navigation');
+
+function menuOpen() {
+    menu.style.display = "block";
+    toolbar.style.display = "none";
+}
