8c9b159e4c7ed09a8060664484082530393dc529
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-08 21:08:42 +0300

    activate error massege for email input

---
+++ form-validation.js
@@ -0,0 +1,13 @@
+const submit = document.getElementById('formSubmit');
+
+function submitForm() {
+    let input = document.getElementById('emailInput').value;
+    let error = document.getElementById('error');
+
+    if (input.toLowerCase() !== input) {
+        submit.type = "button";
+        error.innerHTML = "Please enter a valid email address!";
+    } else {
+        submit.type = "submit";
+    }
+}
