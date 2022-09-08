f0a2d7bf269130c59b9e674497f182aa2965491f
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-08 22:16:24 +0300

    fix issues with card button

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ const content = document.createElement("p");
 const container = document.createElement("div");
 const division = document.createElement("div");
 const closeBtn = document.createElement("button");
+const divDesBtn = document.createElement("div");
+const mainDiv = document.createElement("div");
 const btnImages = [{
   name: "See Live",
   path: "Assets/Icon-live.png",
@@ -36,14 +38,18 @@ image.classList.add('popup-image');
 content.classList.add('popup-text');
 division.classList.add('popup-btn-container');
 closeBtn.classList.add('popup-close-btn');
+divDesBtn.classList.add('des-btn');
+mainDiv.classList.add('main-div');
 
 container.setAttribute('id', "container");
-headerTwo.setAttribute('id','popupHeader');
-popupList.setAttribute('id','popupList');
-image.setAttribute('id','popupImage');
-content.setAttribute('id','popupText');
-division.setAttribute('id','popupBtnContainer');
-closeBtn.setAttribute('id','popupCloseBtn');
+headerTwo.setAttribute('id', "popupHeader");
+popupList.setAttribute('id', "popupList");
+image.setAttribute('id', "popupImage");
+content.setAttribute('id', "popupText");
+division.setAttribute('id', "popupBtnContainer");
+closeBtn.setAttribute('id', "popupCloseBtn");
+divDesBtn.setAttribute('id', "divDesBtn");
+mainDiv.setAttribute('id', "mainDiv");
 
 actionBtn.forEach((btn, i) => (
   btn.setAttribute('id', data[i].id)
@@ -54,97 +60,97 @@ for (let i = 0; i < workCards.length; i++) {
   workCards[i].children[0].innerHTML = data[i].name;
   workCards[i].children[1].innerHTML = data[i].description;
 }
-//place cardClicked function here
 
 function cardClicked() {
-    mainContainer.style.filter = "blur(5px)";
-    element.style.display = "flex";
-    element.style.position = "fixed";
-  
-    let elemId = this.event.target.id;
-    let selectedProject = data.find(element => elemId == element.id)
-  
-    for (let i = 0; i < selectedProject.technologies.length; i++) {
-      let popupItem = document.createElement("li");
-      popupItem.classList.add('popup-item');
-      popupItem.setAttribute('id', "popupItem");
-      popupItem.innerHTML = selectedProject.technologies[i];
-      popupList.appendChild(popupItem);
-    }
-    
-    headerTwo.innerHTML = selectedProject.name;
-    image.src = selectedProject.thumbnails
-    image.alt = selectedProject.name + " image";
-    content.innerHTML = selectedProject.content;
-  
-    for (let i = 0; i < btnImages.length; i++) {
-      let popupButton = document.createElement("a");
-      let btnIcon = document.createElement("img");
-  
-      popupButton.classList.add('popup-btn');
-      popupButton.setAttribute('id', "popupButton");
-      btnIcon.setAttribute('id', "btnIcon");
-      popupButton.innerHTML = btnImages[i].name;
-      popupButton.target = "_blank";
-      if (btnImages[i].name === "See Live") {
-        popupButton.href = selectedProject.live
-      }
-      if (btnImages[i].name === "See Source") {
-        popupButton.href = selectedProject.source
-      }
-      
-      btnIcon.src = btnImages[i].path;
-      btnIcon.alt = "Action Icon";
-      popupButton.appendChild(btnIcon);
-      division.appendChild(popupButton);
-    };
-  
-    function removeElements() {
-      let rmvClose = document.getElementById("popupCloseBtn");
-      let rmvCloseIcon = document.getElementById("closeIcon");
-      let rmvPopupItem = document.getElementById("popupItem");
-      let itemParent = rmvPopupItem.parentNode;
-      let rmvPopupList = document.getElementById("popupList");
-      
-      rmvClose.parentNode.removeChild(rmvClose);
-      rmvCloseIcon.parentNode.removeChild(rmvCloseIcon);
-      rmvPopupList.parentNode.removeChild(rmvPopupList);
-      
-      for (let i = 0; i < selectedProject.technologies.length; i++) {
-        itemParent.removeChild(itemParent.firstChild);
-      } 
-      
-      for (let i = 0; i < 2; i++) {
-        let rmvPopupButton = document.getElementById("popupButton");
-        let rmvBtnIcon = document.getElementById("btnIcon");
-        rmvBtnIcon.parentNode.removeChild(rmvBtnIcon);
-        rmvPopupButton.parentNode.removeChild(rmvPopupButton);
-      }
-    }
-  
-    function popupClose() {
-      mainContainer.style.filter = "none";
-      element.style.display = "none";
-      element.style.position = "static";
-      removeElements();
-    } 
-  
-    function closeBtnAppend() {
-      let closeIcon = document.createElement('img');
-      closeIcon.setAttribute('id', "closeIcon");
-      closeIcon.src = "Assets/Icon.png";
-      closeIcon.alt = "close icon";
-      closeBtn.appendChild(closeIcon);
-      closeBtn.onclick = popupClose;
-    }
-  
-    closeBtnAppend();
-    element.appendChild(container);
-    container.appendChild(headerTwo);
-    container.appendChild(closeBtn);
-    container.appendChild(popupList);
-    container.appendChild(image);
-    container.appendChild(content);
-    container.appendChild(division);
+  mainContainer.style.filter = "blur(5px)";
+  element.style.display = "flex";
+  element.style.position = "fixed";
+
+  let elemId = this.event.target.id;
+  let selectedProject = data.find(element => elemId == element.id)
+
+  for (let i = 0; i < selectedProject.technologies.length; i++) {
+    let popupItem = document.createElement("li");
+    popupItem.classList.add('popup-item');
+    popupItem.setAttribute('id', "popupItem");
+    popupItem.innerHTML = selectedProject.technologies[i];
+    popupList.appendChild(popupItem);
   }
-  
+
+  headerTwo.innerHTML = selectedProject.name;
+  image.src = selectedProject.thumbnails
+  image.alt = selectedProject.name + " image";
+  content.innerHTML = selectedProject.content;
+
+  for (let i = 0; i < btnImages.length; i++) {
+    let popupButton = document.createElement("a");
+    let btnIcon = document.createElement("img");
+
+    popupButton.classList.add('popup-btn');
+    popupButton.setAttribute('id', "popupButton");
+    btnIcon.setAttribute('id', "btnIcon");
+    popupButton.innerHTML = btnImages[i].name;
+    popupButton.target = "_blank";
+    if (btnImages[i].name === "See Live") {
+      popupButton.href = selectedProject.live
+    }
+    if (btnImages[i].name === "See Source") {
+      popupButton.href = selectedProject.source
+    }
+
+    btnIcon.src = btnImages[i].path;
+    btnIcon.alt = "Action Icon";
+    popupButton.appendChild(btnIcon);
+    division.appendChild(popupButton);
+  };
+
+  function removeElements() {
+    let rmvClose = document.getElementById("popupCloseBtn");
+    let rmvCloseIcon = document.getElementById("closeIcon");
+    let rmvPopupItem = document.getElementById("popupItem");
+    let itemParent = rmvPopupItem.parentNode;
+    let rmvPopupList = document.getElementById("popupList");
+
+    rmvClose.parentNode.removeChild(rmvClose);
+    rmvCloseIcon.parentNode.removeChild(rmvCloseIcon);
+    rmvPopupList.parentNode.removeChild(rmvPopupList);
+
+    for (let i = 0; i < selectedProject.technologies.length; i++) {
+      itemParent.removeChild(itemParent.firstChild);
+    }
+
+    for (let i = 0; i < 2; i++) {
+      let rmvPopupButton = document.getElementById("popupButton");
+      let rmvBtnIcon = document.getElementById("btnIcon");
+      rmvBtnIcon.parentNode.removeChild(rmvBtnIcon);
+      rmvPopupButton.parentNode.removeChild(rmvPopupButton);
+    }
+  }
+
+  function popupClose() {
+    mainContainer.style.filter = "none";
+    element.style.display = "none";
+    element.style.position = "static";
+    removeElements();
+  }
+
+  function closeBtnAppend() {
+    let closeIcon = document.createElement('img');
+    closeIcon.setAttribute('id', "closeIcon");
+    closeIcon.src = "Assets/Icon.png";
+    closeIcon.alt = "close icon";
+    closeBtn.appendChild(closeIcon);
+    closeBtn.onclick = popupClose;
+  }
+
+  closeBtnAppend();
+  element.appendChild(container);
+  container.appendChild(closeBtn);
+  container.appendChild(headerTwo);
+  container.appendChild(popupList);
+  mainDiv.appendChild(image);
+  mainDiv.appendChild(divDesBtn)
+  divDesBtn.appendChild(content);
+  divDesBtn.appendChild(division);
+  container.appendChild(mainDiv);
+}
