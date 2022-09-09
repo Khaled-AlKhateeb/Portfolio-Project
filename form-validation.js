193947362acacb71d552a7f0a44768fe0faec9f3
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-09 09:07:24 +0300

    fix functionality

--- form-validation.js
+++ form-validation.js
@@ -4,6 +4,7 @@ function submitForm() {
     let input = document.getElementById('emailInput').value;
     let error = document.getElementById('error');
 
+    console.log(input);
     if (input.toLowerCase() !== input) {
         submit.type = "button";
         error.innerHTML = "Please enter a valid email address!";
@@ -12,3 +13,4 @@ function submitForm() {
         error.innerHTML = "";
     }
 }
+
